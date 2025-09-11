51665c1fc34e94a8f133e1d2f12920369467b982
Caio Henrique Ramos de Souza <caiohenriquebr12@gmail.com>
2025-09-11 00:05:51 -0300

    adicionando o commit inicial do curso de java POO udemy

---
+++ OlaMundo.java
@@ -0,0 +1,12 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+public class OlaMundo {
+   public OlaMundo() {
+   }
+
+   public static void main(String[] args) {
+      System.out.print("olá rapaziada");
+      System.out.println("Olá Mundo!");
+   }
+}
+
+aaaaaaaaaaaaaaaaaaaa
